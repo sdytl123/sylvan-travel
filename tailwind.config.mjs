4fa611da1a63792641a9ecd0921eeaf7aae539e2
sdytl123 <sdytl123@gmail.com>
2026-09-26 13:02:48 +1000

    ui version

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'sylvan-blue': '#1a365d',
+        'sylvan-gold': '#d4af37',
+      },
+    },
+  },
+  plugins: [require('daisyui')],
+}
