f80f96acf61198d58d9c846448d594c3059a3262
sdytl123 <sdytl123@gmail.com>
2026-09-26 22:57:19 +1000

    11

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  integrations: [tailwind()],
   site: 'https://sylvantravel.com',
   outDir: 'dist',
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,4 @@
+import daisyui from 'daisyui';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -9,5 +10,5 @@ export default {
       },
     },
   },
-  plugins: [require('daisyui')],
+  plugins: [daisyui],
 }
